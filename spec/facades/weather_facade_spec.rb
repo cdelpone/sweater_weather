e905dd120ff5ed3822a798a43a7d87142b152fcf
Christina Delpone <cdelpone@gmail.com>
2021-11-17 15:30:19 -0700

    test for unnecessary data to not be present

--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe 'WeatherFacade', :vcr do
     location = 'Denver,CO'
     result = WeatherFacade.get_weather_data(location)
     expect(result).to be_a Hash
+    expect(result.size).to eq(3)
+    expect(result).to have_key :current_weather
+    expect(result).to have_key :hourly_weather
+    expect(result).to have_key :daily_weather
   end
 
   it 'returns a hash with current, hourly, and daily weather data' do
@@ -12,7 +16,44 @@ RSpec.describe 'WeatherFacade', :vcr do
     location_data = MapQuestFacade.get_lat_lng(location)
     weather_data = WeatherService.weather_data(location_data.latitude, location_data.longitude)
     result = WeatherFacade.attributes(weather_data)
+
     expect(result).to be_a Hash
+    expect(result.size).to eq(3)
+
+    expect(result).to have_key :current_weather
+    expect(result[:current_weather]).to be_a Current
+    current = result[:current_weather]
+    expect(current.conditions).to be_a String
+    expect(current.datetime).to be_a Time
+    expect(current.feels_like).to be_a Float
+    expect(current.humidity).to be_a Numeric
+    expect(current.icon).to be_a String
+    expect(current.sunrise).to be_a Time
+    expect(current.sunset).to be_a Time
+    expect(current.temperature).to be_a Float
+    expect(current.uvi).to be_a Float
+    expect(current.visibility).to be_a Numeric
+
+    expect(result).to have_key :hourly_weather
+    expect(result[:hourly_weather]).to be_an Array
+    expect(result[:hourly_weather].size).to eq(8)
+    hourly = result[:hourly_weather][0]
+    expect(hourly.conditions).to be_a String
+    expect(hourly.time).to be_a String
+    expect(hourly.icon).to be_a String
+    expect(hourly.temperature).to be_a Float
+
+    expect(result).to have_key :daily_weather
+    expect(result[:daily_weather]).to be_an Array
+    expect(result[:daily_weather].size).to eq(5)
+    daily = result[:daily_weather][0]
+    expect(daily.conditions).to be_a String
+    expect(daily.date).to be_a String
+    expect(daily.icon).to be_a String
+    expect(daily.max_temp).to be_a Float
+    expect(daily.min_temp).to be_a Float
+    expect(daily.sunrise).to be_a Time
+    expect(daily.sunset).to be_a Time
   end
 
    it 'hourly_weather' do
--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -1,67 +1,70 @@
 require 'rails_helper'
 
 RSpec.describe 'ForecastController', :vcr do
-  it 'makes a successful call' do
-    location = 'denver,CO'
-
+  before :each do
+    location = 'Denver,CO'
     get "/api/v1/forecast?location=#{location}"
-
+    @forecast_data = JSON.parse(response.body, symbolize_names: true)
+    @current_weather = @forecast_data[:current_weather]
+    @hourly_weather = @forecast_data[:hourly_weather][0]
+    @daily_weather = @forecast_data[:daily_weather][0]
+  end
+  it 'makes a successful call' do
     expect(response).to be_successful
   end
 
   it 'returns expected hash keys' do
-    location = 'Denver,CO'
-    get "/api/v1/forecast?location=#{location}"
+    expect(@forecast_data).to be_a Hash
+    expect(@forecast_data).to have_key(:current_weather)
+    expect(@forecast_data[:current_weather]).to be_a Hash
 
-    forecast_data = JSON.parse(response.body, symbolize_names: true)
+    expect(@forecast_data).to have_key(:hourly_weather)
+    expect(@forecast_data[:hourly_weather]).to be_an Array
+    expect(@forecast_data[:hourly_weather].size).to eq(8)
 
-    expect(forecast_data).to be_a Hash
-    expect(forecast_data).to have_key(:current_weather)
-    expect(forecast_data[:current_weather]).to be_a Hash
-    expect(forecast_data).to have_key(:hourly_weather)
-    expect(forecast_data[:hourly_weather]).to be_an Array
-    expect(forecast_data[:hourly_weather].size).to eq(8)
-    expect(forecast_data).to have_key(:daily_weather)
-    expect(forecast_data[:daily_weather]).to be_an Array
-    expect(forecast_data[:daily_weather].size).to eq(5)
-    current_weather = forecast_data[:current_weather]
-    expect(current_weather).to be_a Hash
-    expect(current_weather).to have_key(:datetime)
-    expect(current_weather).to have_key(:sunrise)
-    expect(current_weather).to have_key(:sunset)
-    expect(current_weather).to have_key(:temperature)
-    expect(current_weather).to have_key(:feels_like)
-    expect(current_weather).to have_key(:humidity)
-    expect(current_weather).to have_key(:uvi)
-    expect(current_weather).to have_key(:visibility)
-    expect(current_weather).not_to have_key(:weather)
-    expect(current_weather).to have_key(:conditions)
-    expect(current_weather).to have_key(:icon)
+    expect(@forecast_data).to have_key(:daily_weather)
+    expect(@forecast_data[:daily_weather]).to be_an Array
+    expect(@forecast_data[:daily_weather].size).to eq(5)
 
-    hourly_weather = forecast_data[:hourly_weather][0]
-    expect(hourly_weather).to be_a Hash
-    expect(hourly_weather).to have_key(:time)
-    expect(hourly_weather).to have_key(:temperature)
-    expect(hourly_weather).not_to have_key(:weather)
-    expect(hourly_weather).to have_key(:conditions)
-    expect(hourly_weather).to have_key(:icon)
+    expect(@current_weather).to be_a Hash
+    expect(@current_weather).to have_key(:datetime)
+    expect(@current_weather).to have_key(:sunrise)
+    expect(@current_weather).to have_key(:sunset)
+    expect(@current_weather).to have_key(:temperature)
+    expect(@current_weather).to have_key(:feels_like)
+    expect(@current_weather).to have_key(:humidity)
+    expect(@current_weather).to have_key(:uvi)
+    expect(@current_weather).to have_key(:visibility)
+    expect(@current_weather).to have_key(:conditions)
+    expect(@current_weather).to have_key(:icon)
+
+    expect(@hourly_weather).to be_a Hash
+    expect(@hourly_weather).to have_key(:time)
+    expect(@hourly_weather).to have_key(:temperature)
+    expect(@hourly_weather).to have_key(:conditions)
+    expect(@hourly_weather).to have_key(:icon)
 
 
-    daily_weather = forecast_data[:daily_weather][0]
-    expect(daily_weather).to be_a Hash
-    expect(daily_weather).to have_key(:date)
-    expect(daily_weather).to have_key(:sunrise)
-    expect(daily_weather).to have_key(:sunset)
-
-    expect(daily_weather).not_to have_key(:temperature)
-    expect(daily_weather).to have_key(:min_temp)
-    expect(daily_weather).to have_key(:max_temp)
-
-    expect(daily_weather).not_to have_key(:weather)
-    expect(daily_weather).to have_key(:conditions)
-    expect(daily_weather).to have_key(:icon)
+    expect(@daily_weather).to be_a Hash
+    expect(@daily_weather).to have_key(:date)
+    expect(@daily_weather).to have_key(:sunrise)
+    expect(@daily_weather).to have_key(:sunset)
+    expect(@daily_weather).to have_key(:min_temp)
+    expect(@daily_weather).to have_key(:max_temp)
+    expect(@daily_weather).to have_key(:conditions)
+    expect(@daily_weather).to have_key(:icon)
   end
 
   it 'does not return unnecessary data' do
+    expect(@current_weather).not_to have_key(:weather)
+    expect(@hourly_weather).not_to have_key(:weather)
+    expect(@daily_weather).not_to have_key(:temperature)
+    expect(@daily_weather).not_to have_key(:feels_like)
+    expect(@daily_weather).not_to have_key(:weather)
+    expect(@forecast_data).not_to have_key(:minutely)
+    expect(@forecast_data).not_to have_key(:lat)
+    expect(@forecast_data).not_to have_key(:lon)
+    expect(@forecast_data).not_to have_key(:timezone)
+    expect(@forecast_data).not_to have_key(:timezone_offset)
   end
 end
