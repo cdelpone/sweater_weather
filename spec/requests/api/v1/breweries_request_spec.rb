754953def6bc4bed6725451a3bc1a3b6052951b8
Christina Delpone <cdelpone@gmail.com>
2021-11-16 19:15:29 -0700

    test successful call

---
+++ spec/requests/api/v1/breweries_request_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe 'BreweriesController' do
+  it 'makes a successful call' do
+    location = 'Denver,CO'
+    quantity = 5
+    # https://api.openbrewerydb.org/breweries?by_city=denver&by_state=co
+    get "/api/v1/breweries?location=denver,co&quantity=5"
+    # get "/api/v1/breweries?location=#{location}&quantity=#{quantity}"
+
+    expect(response).to be_successful
+
+    # breweries = JSON.parse(response.body, symbolize_names: true)
+    # expect(breweries).to be_a Hash
+  end
+
+  it 'returns expected keys and data types' do
+
+  end
+
+  it 'does not return unnecessary data' do
+
+  end
+end
+# {
+#   "data": {
+#     "id": null,
+#     "type": "breweries",
+#     "attributes": {
+#       "destination": "denver,co",
+#       "forecast": {
+#         "summary": "Cloudy with a chance of meatballs",
+#         "temperature": "83 F"
+#       },
+#       "breweries": [
+#         {
+#           "id": "denver-beer-co-33,
+#           "name": "Denver Beer Co Olde Town Arvada",
+#           "brewery_type": "micro"
+#         },
+#         {
+#           "id": "new-image-3342,
+#           "name": "New Image Brewing Co",
+#           "brewery_type": "brewpub"
+#         },
+#         { ... same format for breweries 3, 4 and 5 ... }
+#       ]
+#     }
+#   }
+# }
