1f90f8fca347e624aca4f8de0c6a62871f00d31c
Christina Delpone <cdelpone@gmail.com>
2022-01-13 18:11:33 -0700

    Complete section 1b for background images

--- spec/facades/background_facade_spec.rb
+++ spec/facades/background_facade_spec.rb
@@ -4,15 +4,14 @@ require 'rails_helper'
 
 RSpec.describe 'BackgroundFacade', :vcr do
   it 'returns an image object' do
-    location_params = 'Denver,CO'
-    image = BackgroundFacade.image_data(location_params)
+    location = { location: 'Denver,CO' }
+    image_data = ImagesService.search_by_location(location)
+    image = BackgroundFacade.image_data(location)
 
     expect(image).to be_an Image
     expect(image.location).to be_a String
+    expect(image.location).to eq("Denver,CO")
     expect(image.raw_url).to be_a String
-    # expect(image.credit).to be_a Hash
-    # expect(image.source).to be_a String
-    # expect(image.photographer).to be_a String
-    # expect(image.photographer_profile_link).to be_a String
+    expect(image.credit).to be_a Hash
   end
 end
--- spec/requests/api/v1/backgrounds_request_spec.rb
+++ spec/requests/api/v1/backgrounds_request_spec.rb
@@ -6,68 +6,30 @@ RSpec.describe 'BackgroundsController', :vcr do
 
     get "/api/v1/backgrounds?location=#{location}"
 
-    image = JSON.parse(response.body, symbolize_names: true)
-
     expect(response).to be_successful
-    expect(response).to be 200
+    expect(response.status).to be 200
   end
 
-  it 'returns a serialized image' do
+  it 'serialized the image with certain attributes' do
     location = 'Denver,CO'
 
     get "/api/v1/backgrounds?location=#{location}"
 
     image = JSON.parse(response.body, symbolize_names: true)
 
-    expect(image).to have_key[:data]
-    expect(image[:data]).to have_key[:id]
-    expect(image[:data]).to have_key[:type]
+    expect(image).to have_key :data
+    expect(image[:data]).to have_key :id
+    expect(image[:data]).to have_key :type
     expect(image[:data][:type]).to eq("image")
-    expect(image[:data]).to have_key[:attributes]
-    expect(image[:data][:attributes]).to have_key[:raw_url]
-    expect(image[:data][:attributes]).to have_key[:source]
-    expect(image[:data][:attributes]).to have_key[:photographer]
-    expect(image[:data][:attributes]).to have_key[:photographer_profile_link]
-    expect(image[:data][:attributes]).to have_key[:location]
+    expect(image[:data]).to have_key :attributes
+    expect(image[:data][:attributes]).to have_key :raw_url
+    expect(image[:data][:attributes]).to have_key :location
+    expect(image[:data][:attributes]).to have_key :credit
+    expect(image[:data][:attributes][:credit]).to have_key :source
+    expect(image[:data][:attributes][:credit]).to have_key :photographer
+    expect(image[:data][:attributes][:credit]).to have_key :photographer_profile_link
     expect(image[:data][:attributes][:location]).to eq(location)
-    # When displaying a photo from Unsplash, your application must attribute
-    # Unsplash,
-    # the Unsplash photographer,
-    # contain a link back to their Unsplash profile.
-
-    # Photo by Annie Spratt on Unsplash
-    # photpgrapher profile link:  # https://unsplash.com/@anniespratt?utm_source=your_app_name&utm_medium=referral
-    # Unsplash link:  # https://unsplash.com/?utm_source=your_app_name&utm_medium=referral
   end
-
-  it 'returns expected keys' do
-    # expect(results).to have_key(:image_url)
-    # expect(results).to have_key(:location)
-    #
-    # status: 200
-    # body:
-    #
-    # {
-    #   "data": {
-    #     "type": "image",
-    #     "id": null,
-    #     "attributes": {
-    #       "image": {
-    #         "location": "denver,co",
-    #         "image_url": "https://pixabay.com/get/54e6d4444f50a814f1dc8460962930761c38d6ed534c704c7c2878dd954dc451_640.jpg",
-    #         "credit": {
-    #           "source": "https://unsplash.com/?utm_source=your_app_name&utm_medium=referral",
-    #           "photographer": "John Smith",
-    #           "photographer_profile_link": "https://unsplash.com/@johnsmith?utm_source=your_app_name&utm_medium=referral"
-    #         }
-    #       }
-    #     }
-    #   }
-    # }
-  end
-
-  # it 'returns expected key values' do
-  # end
 
   # it 'returns expected key value data types' do
   # end
@@ -75,15 +37,9 @@ RSpec.describe 'BackgroundsController', :vcr do
   # it 'does not return unnecessary data' do
   # end
 end
-# expect(@image_data).to be_a Hash
-# expect(@).to be_a Hash
-# expect(@.size).to eq(8)
-# expect(@).to have_key(:)
 
 # expect().to be_an Array
 # expect().to be_a Time
 # expect().to be_a Float
 # expect().to be_a Numeric
 # expect().to be_a String
-
-# expect(@).not_to have_key(:)
