88cb3421377bc750b5b15b71f06c28e632816a8b
Christina Delpone <cdelpone@gmail.com>
2022-01-16 20:16:20 -0700

    Remove comments

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,11 +4,9 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-    # if there's not a user, create it and then render json
     unless user
       user = User.create!(user_params.merge({ api_key: User.api_key }))
     end
-    # if there is a user, redirect to the sessions controller (or sessions method?) to authenticate
     render json: UserSerializer.new(user), status: 201
   end
 
