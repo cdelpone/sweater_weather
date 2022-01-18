1f12aead49d1e93d817f6404a90876cff17bcebc
Christina Delpone <cdelpone@gmail.com>
2022-01-18 15:25:21 -0700

    refactor mapquest service

--- app/services/map_quest_service.rb
+++ app/services/map_quest_service.rb
@@ -1,14 +1,20 @@
 class MapQuestService
   class << self
-    def mapquest_data(location)
-      conn = Faraday.new(url: 'http://www.mapquestapi.com')
+    def conn
+      Faraday.new(url: 'http://www.mapquestapi.com')
+    end
 
+    def mapquest_response(location)
       response = conn.get('/geocoding/v1/address') do |faraday|
         faraday.params['key'] = ENV['MAPQUEST_KEY']
         faraday.params['location'] = location
       end
 
       JSON.parse(response.body, symbolize_names: true)
+    end
+
+    def mapquest_data(location)
+      mapquest_response(location)[:results].first
     end
   end
 end
--- spec/services/map_quest_service_spec.rb
+++ spec/services/map_quest_service_spec.rb
@@ -1,27 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe 'MapQuestService', :vcr do
-  it 'can get all map quest data for a given location' do
+  it 'receives all data for a given location' do
     location = 'Denver,CO'
+    mapquest_response = MapQuestService.mapquest_response(location)
 
+    expect(mapquest_response).to be_a Hash
+    expect(mapquest_response[:results]).to be_an Array
+
+    results = mapquest_response[:results][0]
+
+    expect(results).to have_key(:providedLocation)
+    expect(results[:providedLocation]).to have_key(:location)
+    expect(results).to have_key(:locations)
+    expect(results[:locations]).to be_an Array
+    expect(results[:locations][0]).to be_a Hash
+    expect(results[:locations][0]).to have_key(:latLng)
+    expect(results[:locations][0][:latLng]).to be_a Hash
+
+    lat_lng = results[:locations][0][:latLng]
+
+    expect(lat_lng).to be_a Hash
+    expect(lat_lng).to have_key(:lat)
+    expect(lat_lng).to have_key(:lng)
+  end
+
+  it 'returns results key from all data' do
+    location = 'Denver,CO'
     mapquest_data = MapQuestService.mapquest_data(location)
 
     expect(mapquest_data).to be_a Hash
-    expect(mapquest_data[:results]).to be_an Array
+    expect(mapquest_data[:locations]).to be_an Array
 
-    locations_data = mapquest_data[:results][0]
+    expect(mapquest_data).to have_key(:providedLocation)
+    expect(mapquest_data[:providedLocation]).to have_key(:location)
+    expect(mapquest_data[:providedLocation][:location]).to eq(location)
 
-    expect(locations_data).to have_key(:providedLocation)
-    expect(locations_data[:providedLocation]).to have_key(:location)
-    expect(locations_data[:providedLocation][:location]).to eq(location)
+    locations_data = mapquest_data[:locations][0]
 
-    expect(locations_data).to have_key(:locations)
-    expect(locations_data[:locations]).to be_an Array
-    expect(locations_data[:locations][0]).to be_a Hash
-    expect(locations_data[:locations][0]).to have_key(:latLng)
-    expect(locations_data[:locations][0][:latLng]).to be_a Hash
+    expect(mapquest_data).to have_key(:locations)
+    expect(mapquest_data[:locations]).to be_an Array
+    expect(mapquest_data[:locations][0]).to be_a Hash
+    expect(mapquest_data[:locations][0]).to have_key(:latLng)
+    expect(mapquest_data[:locations][0][:latLng]).to be_a Hash
 
-    lat_lng = locations_data[:locations][0][:latLng]
+    lat_lng = mapquest_data[:locations][0][:latLng]
 
     expect(lat_lng).to be_a Hash
     expect(lat_lng).to have_key(:lat)
