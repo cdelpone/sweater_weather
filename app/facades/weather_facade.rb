f96b490e4a5812fbf506046a7f31cd2bae0123d5
Christina Delpone <cdelpone@gmail.com>
2022-01-18 14:28:37 -0700

    refactor weather facade to return objects

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -1,41 +1,24 @@
 class WeatherFacade
   class << self
-
-    def get_weather_data(location)
+    def weather_data(location)
       location_data = MapQuestFacade.get_lat_lng(location)
-      weather_data = WeatherService.weather_data(location_data.latitude, location_data.longitude)
-      weather_collection(weather_data)
+      WeatherService.weather_data(location_data.latitude, location_data.longitude)
     end
 
-    def weather_collection(weather_data)
-      [Current.new(weather_data), daily_weather_poro(weather_data), hourly_weather_poro(weather_data)]
+    def current_weather(location)
+      Current.new(weather_data(location))
     end
 
-    def daily_weather_poro(weather_data)
-      weather_data[:daily][0..4].map do |data|
+    def daily_weather(location)
+      weather_data(location)[:daily][0..4].map do |data|
         Day.new(data)
       end
     end
 
-    def hourly_weather_poro(weather_data)
-      weather_data[:hourly][0..7].map do |data|
+    def hourly_weather(location)
+      weather_data(location)[:hourly][0..7].map do |data|
         Hour.new(data)
       end
     end
   end
 end
-# def for_controller(location)
-#   weather_data = get_weather_data(location)
-#   create_poros(weather_data)
-# end
-# def get_weather_data(location)
-#   location_data = MapQuestFacade.get_lat_lng(location)
-#   weather_data = WeatherService.weather_data(location_data.latitude, location_data.longitude)
-# end
-# def create_poros(weather_data)
-  # {
-  #   current: Current.new(weather_data[:current]),
-  #   daily: daily_weather_poro(weather_data),
-  #   hourly: hourly_weather_poro(weather_data)
-  # }
-# end
--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -3,74 +3,38 @@ require 'rails_helper'
 RSpec.describe 'WeatherFacade', :vcr do
     before :each do
       @location = 'Denver,CO'
-      @location_data = MapQuestFacade.get_lat_lng(@location)
-      @weather_data = WeatherService.weather_data(@location_data.latitude, @location_data.longitude)
     end
 
     it 'returns all weather data' do
-      weather_data = WeatherFacade.get_weather_data(@location)
+      weather_data = WeatherFacade.weather_data(@location)
 
-      expect(weather_data).to be_an Array
-      expect(weather_data.size).to eq(3)
+      expect(weather_data).to be_a Hash
+      expect(weather_data).to have_key :lat
+      expect(weather_data).to have_key :lon
+      expect(weather_data).to have_key :timezone
+      expect(weather_data).to have_key :timezone_offset
+      expect(weather_data).to have_key :current
+      expect(weather_data).to have_key :hourly
+      expect(weather_data).to have_key :daily
     end
 
-    it 'creates an array of poros' do
-      collection = WeatherFacade.weather_collection(@weather_data)
-      expect(collection).to be_an Array
-      expect(collection.size).to eq(3)
-      expect(collection[0]).to be_a Current
-      expect(collection[1]).to be_an Array
-      expect(collection[1].first).to be_a Day
-      expect(collection[2]).to be_an Array
-      expect(collection[2].first).to be_an Hour
-    end
+   it 'current_weather' do
+     current = WeatherFacade.current_weather(@location)
+
+     expect(current).to be_a Current
+     expect(current.conditions).to be_a String
+     expect(current.datetime).to be_a Time
+   end
 
    it 'hourly_weather' do
-     hourly_objects = WeatherFacade.hourly_weather_poro(@weather_data)
-     expect(hourly_objects).to be_an Array
-     expect(hourly_objects.first).to be_a Hour
+     hour = WeatherFacade.hourly_weather(@location)
+
+     expect(hour.first).to be_a Hour
    end
 
    it 'daily_weather' do
-     daily_objects = WeatherFacade.daily_weather_poro(@weather_data)
+     day = WeatherFacade.daily_weather(@location)
 
-     expect(daily_objects).to be_an Array
-     expect(daily_objects.first).to be_a Day
+     expect(day.first).to be_a Day
    end
-
-   it 'returns objects' do
-     # expect(result).to have_key :current_weather
-     # expect(result[:current_weather]).to be_a Current
-     # current = result[:current_weather]
-     # expect(current.conditions).to be_a String
-     # expect(current.datetime).to be_a Time
-     # expect(current.feels_like).to be_a Float
-     # expect(current.humidity).to be_a Numeric
-     # expect(current.icon).to be_a String
-     # expect(current.sunrise).to be_a Time
-     # expect(current.sunset).to be_a Time
-     # expect(current.temperature).to be_a Float
-     # expect(current.uvi).to be_a Numeric
-     # expect(current.visibility).to be_a Numeric
-     #
-     # expect(result).to have_key :hourly_weather
-     # expect(result[:hourly_weather]).to be_an Array
-     # hourly = result[:hourly_weather][0]
-     # expect(hourly.conditions).to be_a String
-     # expect(hourly.time).to be_a String
-     # expect(hourly.icon).to be_a String
-     # expect(hourly.temperature).to be_a Float
-     #
-     # expect(result).to have_key :daily_weather
-     # expect(result[:daily_weather]).to be_an Array
-     # daily = result[:daily_weather][0]
-     # expect(daily.conditions).to be_a String
-     # expect(daily.date).to be_a String
-     # expect(daily.icon).to be_a String
-     # expect(daily.max_temp).to be_a Float
-     # expect(daily.min_temp).to be_a Float
-     # expect(daily.sunrise).to be_a Time
-     # expect(daily.sunset).to be_a Time
-   end
-
 end
