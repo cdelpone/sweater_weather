6e25a14ae245ac5ac9655baf13e71f767cc1f6cc
Christina Delpone <cdelpone@gmail.com>
2022-01-19 13:46:26 -0700

    Add sad path testing

--- spec/facades/map_quest_facade_spec.rb
+++ spec/facades/map_quest_facade_spec.rb
@@ -1,49 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe 'MapQuestFacade', :vcr do
-  it 'returns lat and lon data in a hash' do
-    location = 'Denver,CO'
-    location_data = MapQuestFacade.lat_lon(location)
+  context 'happy paths' do
+    it 'returns lat and lon data in a hash' do
+      location = 'Denver,CO'
+      location_data = MapQuestFacade.lat_lon(location)
 
-    expect(location_data).to be_a Hash
-    expect(location_data[:latitude]).to be_a Float
-    expect(location_data[:longitude]).to be_a Float
-    expect(location_data[:provided_location]).to be_a String
-    expect(location_data[:provided_location]).to eq(location)
+      expect(location_data).to be_a Hash
+      expect(location_data[:latitude]).to be_a Float
+      expect(location_data[:longitude]).to be_a Float
+      expect(location_data[:provided_location]).to be_a String
+      expect(location_data[:provided_location]).to eq(location)
+    end
+
+    it 'returns all location data' do
+      location = 'Denver,CO'
+      mapquest_data = MapQuestFacade.mapquest_geocoding_data(location)
+
+      expect(mapquest_data).to be_a Hash
+    end
+
+    it 'returns a location poro with expected info' do
+      location = 'Denver,CO'
+      location_obj = MapQuestFacade.location_data(location)
+
+      expect(location_obj).to be_a Location
+      expect(location_obj.latitude).to be_a Float
+      expect(location_obj.longitude).to be_a Float
+      expect(location_obj.provided_location).to be_a String
+      expect(location_obj.provided_location).to eq(location)
+    end
+
+    it 'returns travel time between two locations' do
+      origin = 'Denver,CO'
+      destination = 'Estes Park, CO'
+      travel_time = MapQuestFacade.mapquest_travel_time(origin, destination)
+
+      expect(travel_time).to be_a String
+    end
   end
 
-  it 'returns all location data' do
-    location = 'Denver,CO'
-    mapquest_data = MapQuestFacade.mapquest_geocoding_data(location)
+  context 'edge case/sad path' do
+    it 'returns an error message if route is impossible' do
+      origin = 'Denver,CO'
+      destination = 'honolulu, hi'
+      travel_time = MapQuestFacade.mapquest_travel_time(origin, destination)
 
-    expect(mapquest_data).to be_a Hash
-  end
-
-  it 'returns a location poro with expected info' do
-    location = 'Denver,CO'
-    location_obj = MapQuestFacade.location_data(location)
-
-    expect(location_obj).to be_a Location
-    expect(location_obj.latitude).to be_a Float
-    expect(location_obj.longitude).to be_a Float
-    expect(location_obj.provided_location).to be_a String
-    expect(location_obj.provided_location).to eq(location)
-  end
-
-  it 'returns an error message ' do
-    origin = 'Denver,CO'
-    destination = 'honolulu, hi'
-    travel_time = MapQuestFacade.mapquest_travel_time(origin, destination)
-
-    expect(travel_time).to be_a String
-    expect(travel_time).to eq("Impossible route")
-  end
-
-  it 'returns travel time between two locations' do
-    origin = 'Denver,CO'
-    destination = 'Estes Park, CO'
-    travel_time = MapQuestFacade.mapquest_travel_time(origin, destination)
-
-    expect(travel_time).to be_a String
+      expect(travel_time).to be_a String
+      expect(travel_time).to eq("Impossible route")
+    end
   end
 end
--- spec/facades/road_trip_facade_spec.rb
+++ spec/facades/road_trip_facade_spec.rb
@@ -1,13 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe 'RoadTripFacade', :vcr do
-  it 'returns a rt object' do
-    user = User.create(email: 'pesto@fakeemail.com', password: 'password123', password_confirmation: 'password123', api_key: '132w4tergydfiuxbjght')
+  context 'happy paths' do
+    it 'returns a rt object' do
+      user = User.create(email: 'pesto@fakeemail.com', password: 'password123', password_confirmation: 'password123', api_key: '132w4tergydfiuxbjght')
 
-    origin = 'Denver,CO'
-    destination = 'Estes Park, CO'
+      origin = 'Denver,CO'
+      destination = 'Estes Park, CO'
 
-    rt_obj = RoadTripFacade.travel_details(origin, destination)
-    expect(rt_obj).to be_a RoadTrip
+      rt_obj = RoadTripFacade.travel_details(origin, destination)
+
+      expect(rt_obj).to be_a RoadTrip
+      expect(rt_obj.start_city).to eq(origin)
+      expect(rt_obj.end_city).to eq(destination)
+      expect(rt_obj.travel_time).to eq("01:22:19")
+      expect(rt_obj.weather_at_eta).to be_a Hash
+      expect(rt_obj.weather_at_eta).to have_key :conditions
+      expect(rt_obj.weather_at_eta).to have_key :temperature
+    end
+
+    it 'returns a hash with weather conditions and temp for the eta' do
+      user = User.create(email: 'pesto@fakeemail.com', password: 'password123', password_confirmation: 'password123', api_key: '132w4tergydfiuxbjght')
+
+      origin = 'Denver,CO'
+      destination = 'Estes Park, CO'
+
+      hour_hash = RoadTripFacade.forecast_eta(origin, destination)
+
+      expect(hour_hash).to be_a Hash
+      expect(hour_hash).to have_key :conditions
+      expect(hour_hash[:conditions]).to be_a String
+      expect(hour_hash).to have_key :temperature
+      expect(hour_hash[:temperature]).to be_a Float
+    end
+
+    it 'gives the travel time for the route as a string' do
+      user = User.create(email: 'pesto@fakeemail.com', password: 'password123', password_confirmation: 'password123', api_key: '132w4tergydfiuxbjght')
+
+      origin = 'Denver,CO'
+      destination = 'Estes Park, CO'
+
+      travel_time = RoadTripFacade.travel_time(origin, destination)
+
+      expect(travel_time).to be_a String
+      expect(travel_time).to eq("01:22:19")
+    end
+  end
+
+  context 'edge case/sad path' do
+    it 'returns an error if travel time is impossible' do
+      user = User.create(email: 'pesto@fakeemail.com', password: 'password123', password_confirmation: 'password123', api_key: '132w4tergydfiuxbjght')
+
+      origin = 'Denver,CO'
+      destination = 'london, uk'
+
+      travel_time = RoadTripFacade.travel_time(origin, destination)
+
+      expect(travel_time).to be_a String
+      expect(travel_time).to eq("Impossible route")
+    end
   end
 end
